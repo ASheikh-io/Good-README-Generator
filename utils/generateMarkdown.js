4766f5d257fc7508f4b7f3a376be0fff34e52c3d
Ahmed Sheikh <ahsheikh.a@gmail.com>
2020-08-24 00:49:25 -0400

    created js file for license badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,4 @@
-const badge = {
-  "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
-  "Apache 2.0": "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
-  "GPL v3": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
-  "GPF v3": "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
-  "BSD 3": "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
-  "Mozilla Pulic License 2.0": "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
-};
+const badge = require("./licensebadges");
 
 // function to generate markdown for README
 function generateMarkdown(data) {
